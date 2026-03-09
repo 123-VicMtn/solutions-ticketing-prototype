f7cc4662efb737ea0e47683a59ff2316d240008a
123-VicMtn <v.montavon@123-soleil.ch>
2026-03-09 15:10:19 +0100

    feat: Add admin routes for buildings and units

--- start/routes.ts
+++ start/routes.ts
@@ -1,12 +1,3 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
-
 import { middleware } from '#start/kernel'
 import { controllers } from '#generated/controllers'
 import router from '@adonisjs/core/services/router'
@@ -27,4 +18,24 @@ router
   .group(() => {
     router.post('logout', [controllers.Session, 'destroy'])
   })
+  .use(middleware.auth())
+
+router
+  .group(() => {
+    router.get('buildings', [controllers.Buildings, 'index']).as('buildings.index')
+    router.get('buildings/create', [controllers.Buildings, 'create']).as('buildings.create')
+    router.post('buildings', [controllers.Buildings, 'store']).as('buildings.store')
+    router.get('buildings/:id/edit', [controllers.Buildings, 'edit']).as('buildings.edit')
+    router.put('buildings/:id', [controllers.Buildings, 'update']).as('buildings.update')
+    router.delete('buildings/:id', [controllers.Buildings, 'destroy']).as('buildings.destroy')
+
+    router.get('units', [controllers.Units, 'index']).as('units.index')
+    router.get('units/create', [controllers.Units, 'create']).as('units.create')
+    router.post('units', [controllers.Units, 'store']).as('units.store')
+    router.get('units/:id/edit', [controllers.Units, 'edit']).as('units.edit')
+    router.put('units/:id', [controllers.Units, 'update']).as('units.update')
+    router.delete('units/:id', [controllers.Units, 'destroy']).as('units.destroy')
+  })
+  .prefix('admin')
+  .as('admin')
   .use(middleware.auth())
