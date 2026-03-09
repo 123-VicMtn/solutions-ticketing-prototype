ca4204aba41ad9601797f3fc94838df770be5976
123-VicMtn <v.montavon@123-soleil.ch>
2026-03-09 14:44:51 +0100

    feat: Create buildings table migration

---
+++ database/migrations/1761886100000_create_buildings_table.ts
@@ -0,0 +1,22 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'buildings'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').notNullable()
+      table.string('name').notNullable()
+      table.string('address').notNullable()
+      table.string('city').notNullable()
+      table.string('postal_code').notNullable()
+
+      table.timestamp('created_at').notNullable()
+      table.timestamp('updated_at').nullable()
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
