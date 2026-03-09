15d6be97c8b7021c5e35360f0ef23bbc493f145b
123-VicMtn <v.montavon@123-soleil.ch>
2026-03-09 14:47:38 +0100

    feat: Update user validator

--- app/validators/user.ts
+++ app/validators/user.ts
@@ -4,7 +4,8 @@ const email = () => vine.string().email().maxLength(254)
 const password = () => vine.string().minLength(8).maxLength(32)
 
 export const signupValidator = vine.create({
-  fullName: vine.string().nullable(),
+  firstName: vine.string().trim().maxLength(100),
+  lastName: vine.string().trim().maxLength(100),
   email: email().unique({ table: 'users', column: 'email' }),
   password: password().confirmed({
     confirmationField: 'passwordConfirmation',
