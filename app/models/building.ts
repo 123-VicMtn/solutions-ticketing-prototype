8d3322f8319b3d2ff269f28551fb9dab2c067fcf
123-VicMtn <v.montavon@123-soleil.ch>
2026-03-09 14:46:21 +0100

    feat: Implement Building model with properties and relationships

---
+++ app/models/building.ts
@@ -0,0 +1,30 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
+import type { HasMany } from '@adonisjs/lucid/types/relations'
+import Unit from '#models/unit'
+
+export default class Building extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare name: string
+
+  @column()
+  declare address: string
+
+  @column()
+  declare city: string
+
+  @column()
+  declare postalCode: string
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime | null
+
+  @hasMany(() => Unit)
+  declare units: HasMany<typeof Unit>
+}
