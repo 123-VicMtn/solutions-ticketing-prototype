bc957f4b85c25b9ad2a18779c6d7fac7debc5cc1
123-VicMtn <v.montavon@123-soleil.ch>
2026-03-11 10:35:22 +0100

    feat: Enhance user validation by adding createUserValidator and updateUserValidator

--- app/validators/user.ts
+++ app/validators/user.ts
@@ -3,7 +3,16 @@ import vine from '@vinejs/vine'
 const email = () => vine.string().email().maxLength(254)
 const password = () => vine.string().minLength(8).maxLength(32)
 
-export const signupValidator = vine.create({
+export const createUserValidator = vine.create({
+  firstName: vine.string().trim().maxLength(100),
+  lastName: vine.string().trim().maxLength(100),
+  email: email().unique({ table: 'users', column: 'email' }),
+  password: password().confirmed({
+    confirmationField: 'passwordConfirmation',
+  }),
+})
+
+export const updateUserValidator = vine.create({
   firstName: vine.string().trim().maxLength(100),
   lastName: vine.string().trim().maxLength(100),
   email: email().unique({ table: 'users', column: 'email' }),
