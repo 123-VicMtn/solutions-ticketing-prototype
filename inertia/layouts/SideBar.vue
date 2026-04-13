472e0074b70b4f525a8671e41032f6be03feeb1c
123-VicMtn <v.montavon@123-soleil.ch>
2026-04-13 17:13:42 +0200

    feat: replace menu text with SVG icon in sidebar for improved visual appeal

--- inertia/layouts/SideBar.vue
+++ inertia/layouts/SideBar.vue
@@ -13,7 +13,20 @@ const { isAuthenticated, user } = useAuth()
     <div class="drawer-content min-w-0">
       <div class="p-2 lg:hidden">
         <label for="my-drawer-3" class="btn btn-ghost btn-sm drawer-button" aria-label="Open menu">
-          Menu
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
         </label>
       </div>
       <slot />
