c7ebcce418d61357e979fa9c0890839e4c05e5e6
123-VicMtn <v.montavon@123-soleil.ch>
2026-04-13 15:14:30 +0200

    refactor: enhance layout structure by separating foot and sidebar

--- inertia/layouts/default.vue
+++ inertia/layouts/default.vue
@@ -11,24 +11,26 @@ const { user, isAuthenticated } = useAuth()
 </script>
 
 <template>
-  <SideBar v-if="isAuthenticated">
-    <div class="min-h-screen flex flex-col">
-      <Navbar />
+  <div class="min-h-screen flex flex-col">
+    <SideBar v-if="isAuthenticated" class="flex-1">
+      <div class="min-h-full flex flex-col">
+        <Navbar />
+        <main class="mx-auto w-full max-w-7xl flex-1 px-6 py-8">
+          <RoleGate :min="user?.role">
+            <slot />
+          </RoleGate>
+        </main>
+        <Toaster position="top-center" rich-colors />
+      </div>
+    </SideBar>
+
+    <div v-else class="flex-1 flex flex-col">
       <main class="mx-auto w-full max-w-7xl flex-1 px-6 py-8">
-        <RoleGate :min="user?.role">
-          <slot />
-        </RoleGate>
+        <slot />
       </main>
       <Toaster position="top-center" rich-colors />
-      <Footer />
     </div>
-  </SideBar>
 
-  <div v-else class="min-h-screen flex flex-col">
-    <main class="mx-auto w-full max-w-7xl flex-1 px-6 py-8">
-      <slot />
-    </main>
-    <Toaster position="top-center" rich-colors />
     <Footer />
   </div>
 </template>
