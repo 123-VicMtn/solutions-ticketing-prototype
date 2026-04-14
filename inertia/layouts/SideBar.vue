f1097203f8fb232f05018bdc826fa05fa7fe543e
123-VicMtn <v.montavon@123-soleil.ch>
2026-04-14 15:35:45 +0200

    refactor: enhance SideBar layout with new icons and improved UX

--- inertia/layouts/SideBar.vue
+++ inertia/layouts/SideBar.vue
@@ -2,62 +2,84 @@
 import { Link } from '@adonisjs/inertia/vue'
 import RoleGate from '~/components/RoleGate.vue'
 import { useAuth } from '~/composables/use_auth'
+import {
+  BuildingOffice2Icon,
+  HomeIcon,
+  PlusCircleIcon,
+  TicketIcon,
+  UsersIcon,
+} from '@heroicons/vue/24/outline'
 
-const { isAuthenticated, user } = useAuth()
+const { isAuthenticated } = useAuth()
+
+defineProps<{
+  collapsed: boolean
+}>()
 </script>
 
 <template>
-  <div class="drawer">
-    <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
+  <div class="flex flex-1 min-h-0 min-w-0">
+    <aside
+      class="bg-base-200 border-r border-base-300 shrink-0 self-stretch flex flex-col"
+      :class="collapsed ? 'w-auto px-2 py-3' : 'w-auto px-3 py-4'"
+      aria-label="Menu latéral"
+    >
+      <ul class="menu menu-vertical gap-1">
+        <li class="menu-title" v-if="!collapsed"><span>Général</span></li>
 
-    <div class="drawer-content min-w-0">
+        <template v-if="isAuthenticated">
+          <li>
+            <Link route="dashboard.index">
+              <HomeIcon class="size-5" />
+              <span v-if="!collapsed">Tableau de bord</span>
+            </Link>
+          </li>
+        </template>
+
+        <li class="menu-title" v-if="!collapsed"><span>Tickets</span></li>
+        <template v-if="isAuthenticated">
+          <li>
+            <Link route="tickets.index">
+              <TicketIcon class="size-5" />
+              <span v-if="!collapsed">Tous les tickets</span>
+            </Link>
+          </li>
+          <li>
+            <Link route="tickets.create">
+              <PlusCircleIcon class="size-5" />
+              <span v-if="!collapsed">Créer un ticket</span>
+            </Link>
+          </li>
+        </template>
+        <template v-else>
+          <li>
+            <Link route="session.create">
+              <HomeIcon class="size-5" />
+              <span v-if="!collapsed">Se connecter</span>
+            </Link>
+          </li>
+        </template>
+
+        <RoleGate min="manager">
+          <li class="menu-title" v-if="!collapsed"><span>Administration</span></li>
+          <li>
+            <Link route="admin.buildings.index">
+              <BuildingOffice2Icon class="size-5" />
+              <span v-if="!collapsed">Immeubles</span>
+            </Link>
+          </li>
+          <li>
+            <Link route="users.index">
+              <UsersIcon class="size-5" />
+              <span v-if="!collapsed">Utilisateurs</span>
+            </Link>
+          </li>
+        </RoleGate>
+      </ul>
+    </aside>
+
+    <div class="min-w-0 flex-1">
       <slot />
-    </div>
-
-    <div class="drawer-side">
-      <label for="my-drawer-3" aria-label="close sidebar" class="drawer-overlay"></label>
-
-      <aside class="bg-base-200 min-h-full w-auto p-4">
-        <div class="mb-3 px-2">
-          <div class="text-sm font-semibold">Navigation</div>
-        </div>
-
-        <ul class="menu menu-md rounded-box">
-          <li class="menu-title"><span>Général</span></li>
-
-          <template v-if="isAuthenticated">
-            <li><Link route="dashboard.index">Tableau de bord</Link></li>
-          </template>
-
-          <li class="menu-title"><span>Tickets</span></li>
-          <template v-if="isAuthenticated">
-            <li><Link route="tickets.index">Tous les tickets</Link></li>
-            <li><Link route="tickets.create">Créer un ticket</Link></li>
-          </template>
-          <template v-else>
-            <li><Link route="session.create">Se connecter</Link></li>
-            <li><Link route="request_access.create">Demander un accès</Link></li>
-            <li><Link route="new_account.create">Créer un compte</Link></li>
-          </template>
-
-          <RoleGate min="manager">
-            <li class="menu-title"><span>Manager</span></li>
-            <li><Link route="manager.access_requests.index">Demandes d'accès</Link></li>
-          </RoleGate>
-
-          <RoleGate min="manager">
-            <li class="menu-title"><span>Administration</span></li>
-            <li><Link route="admin.buildings.index">Immeubles</Link></li>
-            <li><Link route="admin.units.index">Lots</Link></li>
-            <li><Link route="users.index">Utilisateurs</Link></li>
-          </RoleGate>
-
-          <template v-if="isAuthenticated">
-            <li class="menu-title"><span>Compte</span></li>
-            <li><Link route="users.show" :params="{ id: user?.id }">Mon profil</Link></li>
-          </template>
-        </ul>
-      </aside>
     </div>
   </div>
 </template>
