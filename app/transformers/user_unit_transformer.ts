0de4cdb8f609651f605249c318f647bc883e57d3
123-VicMtn <v.montavon@123-soleil.ch>
2026-04-16 17:11:36 +0200

    feat: add UserUnitTransformer for transforming user unit data into structured objects

---
+++ app/transformers/user_unit_transformer.ts
@@ -0,0 +1,16 @@
+import type UserUnit from '#models/user_unit'
+import { BaseTransformer } from '@adonisjs/core/transformers'
+import UnitTransformer from './unit_transformer.js'
+import UserTransformer from './user_transformer.js'
+
+export default class UserUnitTransformer extends BaseTransformer<UserUnit> {
+  toObject() {
+    return {
+      ...this.pick(this.resource, ['id', 'userId', 'unitId', 'relation']),
+      user: UserTransformer.transform(this.whenLoaded(this.resource.user))?.useVariant(
+        'forUserUnitUser'
+      ),
+      unit: UnitTransformer.transform(this.whenLoaded(this.resource.unit)),
+    }
+  }
+}
