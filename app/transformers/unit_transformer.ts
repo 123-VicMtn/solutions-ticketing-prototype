a3d4cfaf6a58f0b79a46d942f1f09dfb656b660b
123-VicMtn <v.montavon@123-soleil.ch>
2026-04-16 17:44:35 +0200

    feat: enhance UnitTransformer to include userUnits transformation for improved unit data representation

--- app/transformers/unit_transformer.ts
+++ app/transformers/unit_transformer.ts
@@ -1,6 +1,7 @@
 import type Unit from '#models/unit'
 import { BaseTransformer } from '@adonisjs/core/transformers'
 import BuildingTransformer from './building_transformer.js'
+import UserUnitTransformer from './user_unit_transformer.js'
 
 export default class UnitTransformer extends BaseTransformer<Unit> {
   toObject() {
@@ -9,6 +10,9 @@ export default class UnitTransformer extends BaseTransformer<Unit> {
       building: BuildingTransformer.transform(this.whenLoaded(this.resource.building))?.useVariant(
         'forSummary'
       ),
+      userUnits: UserUnitTransformer.transform(
+        this.whenLoaded(this.resource.userUnits)
+      )?.useVariant('forUnitListing'),
     }
   }
 }
