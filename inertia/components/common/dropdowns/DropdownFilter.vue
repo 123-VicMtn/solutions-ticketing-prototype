443ff98334d928de31be3be25a7319fac941445e
123-VicMtn <v.montavon@123-soleil.ch>
2026-04-20 15:41:31 +0200

    feat: enhance DropdownFilter component to support dynamic item types and improve label display logic

--- inertia/components/common/dropdowns/DropdownFilter.vue
+++ inertia/components/common/dropdowns/DropdownFilter.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import Dropdown from './Dropdown.vue'
 
+type DropdownItem = string | { label: string; value: string }
+
 const props = withDefaults(
   defineProps<{
     title: string
-    items: string[]
+    items: DropdownItem[]
     modelValue?: string
     allLabel?: string
   }>(),
@@ -19,6 +21,20 @@ const emit = defineEmits<{
   (e: 'change', value: string): void
 }>()
 
+function itemLabel(item: DropdownItem): string {
+  return typeof item === 'string' ? item : item.label
+}
+
+function itemValue(item: DropdownItem): string {
+  return typeof item === 'string' ? item : item.value
+}
+
+function currentLabel(): string {
+  if (!props.modelValue) return props.allLabel
+  const match = props.items.find((i) => itemValue(i) === props.modelValue)
+  return match ? itemLabel(match) : props.modelValue
+}
+
 function select(value: string) {
   emit('update:modelValue', value)
   emit('change', value)
@@ -26,15 +42,20 @@ function select(value: string) {
 </script>
 
 <template>
-  <Dropdown :title="`${props.title}` + ' : ' + props.modelValue" class="">
+  <Dropdown :title="`${props.title}` + ' : ' + currentLabel()" class="">
     <li>
       <button type="button" :class="{ active: !props.modelValue }" @click="select('')">
         {{ props.allLabel }}
       </button>
     </li>
-    <li v-for="item in props.items" :key="item">
-      <button onclick="document.activeElement.blur()" type="button" :class="{ active: props.modelValue === item }" @click="select(item)">
-        {{ item }}
+    <li v-for="item in props.items" :key="itemValue(item)">
+      <button
+        onclick="document.activeElement.blur()"
+        type="button"
+        :class="{ active: props.modelValue === itemValue(item) }"
+        @click="select(itemValue(item))"
+      >
+        {{ itemLabel(item) }}
       </button>
     </li>
   </Dropdown>
